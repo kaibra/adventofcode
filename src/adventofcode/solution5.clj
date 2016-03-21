11579a267609765002dcb0283d5b78516ee418c2
Kai Brandes <kai.brandes@otto.de>
2016-03-21 17:51:44 +0100

    filter vowels

--- src/adventofcode/solution5.clj
+++ src/adventofcode/solution5.clj
@@ -1,6 +1,19 @@
-(ns adventofcode.solution5)
+(ns adventofcode.solution5
+  (:require [clojure.java.io :as io]))
 
+(defn is-vowel? [c]
+  (not
+    (nil?
+      (get #{\a \e \i \o \u} c))))
+
+(defn at-least-three-vowels [i]
+  (let [vowels (filter is-vowel? (seq i))]
+    (>= (count vowels) 3)))
 
 (defn start []
+  (with-open [rdr (io/reader "resources/5/input.txt")]
+    (-> (line-seq rdr)
+        (filter at-least-three-vowels)
 
-  )
+        (count)
+        (println))))
--- test/adventofcode/solution5_test.clj
+++ test/adventofcode/solution5_test.clj
@@ -3,3 +3,16 @@
     [clojure.test :refer :all]
     [adventofcode.solution5 :as sol]))
 
+
+(deftest vowels
+  (testing ""
+    (is (= true
+           (sol/at-least-three-vowels "aei")))
+    (is (= true
+           (sol/at-least-three-vowels "xazegov")))
+    (is (= true
+           (sol/at-least-three-vowels "aeiouaeiouaeiou")))
+    (is (= false
+           (sol/at-least-three-vowels "x")))
+    (is (= false
+           (sol/at-least-three-vowels "ae")))))
