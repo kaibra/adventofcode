c2dede60f2497f65a9c66fbd0eb86e3a6db6242c
Kai Brandes <kai.brandes@otto.de>
2016-03-23 22:25:15 +0100

    cleanup + fixes for tests

--- test/adventofcode/solution6_test.clj
+++ test/adventofcode/solution6_test.clj
@@ -3,55 +3,51 @@
     [clojure.test :refer :all]
     [adventofcode.solution6 :as sol]))
 
-
 (deftest parsing
   (with-redefs [sol/total-width 10]
-    (testing ""
-      (is (= {:cmd         "turn off"
-              :coordinates [11]}
-             (sol/parse-to-cmd-tasks "turn off 1,1 through 1,1")))
-      (is (= {:cmd         "turn on"
-              :coordinates [21
-                             22
-                             31
-                             32]}
-             (sol/parse-to-cmd-tasks "turn on 1,2 through 2,3")))
-      (is (= {:cmd         "toggle"
-              :coordinates [21
-                            22
-                            31
-                            32]}
-             (sol/parse-to-cmd-tasks "toggle 1,2 through 2,3"))))))
+    (is (= [{:cmd         "turn off"
+             :coordinates [11]}]
+           (sol/parse-to-cmd-tasks "turn off 1,1 through 1,1")))
+    (is (= [{:cmd         "turn on"
+             :coordinates [12 13]}
+            {:cmd         "turn on"
+             :coordinates [22 23]}]
+           (sol/parse-to-cmd-tasks "turn on 1,2 through 2,3")))
+    (is (= [{:cmd         "toggle"
+             :coordinates [12 13]}
+            {:cmd         "toggle"
+             :coordinates [22 23]}]
+           (sol/parse-to-cmd-tasks "toggle 1,2 through 2,3")))))
 
 (deftest building-coordinates
   (with-redefs [sol/total-width 10]
-    (testing ""
-      (is (= [0]
-             (sol/tasks [0 0] [0 0])))
-      (is (= [0 1]
-             (sol/tasks [0 0] [1 0])))
-      (is (= [0 1 2 3 4 5]
-             (sol/tasks [0 0] [5 0])))
-      (is (= [0 10]
-             (sol/tasks [0 0] [0 1])))
-      (is (= [5 15]
-             (sol/tasks [5 0] [5 1])))
-      (is (= [5 6 7 8 9 15 16 17 18 19]
-             (sol/tasks [5 0] [9 1])))))
-  (with-redefs [sol/total-width 3]
-    (testing ""
-      (is (= [0 1 3 4]
-             (sol/tasks [0 0] [1 1])))
-      (is (= [4 5 7 8]
-             (sol/tasks [1 1] [2 2]))))))
+    (is (= [{:cmd         "toggle"
+             :coordinates [0]}]
+           (sol/tasks "toggle" [0 0] [0 0])))
+    (is (= [{:cmd         "toggle"
+             :coordinates [0]}
+            {:cmd         "toggle"
+             :coordinates [10]}]
+           (sol/tasks "toggle" [0 0] [1 0])))
+    (is (= [{:cmd         "turn on"
+             :coordinates [50 51]}
+            {:cmd         "turn on"
+             :coordinates [60 61]}
+            {:cmd         "turn on"
+             :coordinates [70 71]}
+            {:cmd         "turn on"
+             :coordinates [80 81]}
+            {:cmd         "turn on"
+             :coordinates [90 91]}]
+           (sol/tasks "turn on" [5 0] [9 1])))))
 
 (deftest modification
-  (testing "toggle"
-    (let [structure (int-array 3)]
-      (sol/execute-parallel structure {:cmd         "toggle"
-                                       :coordinates [1]})
-      (sol/execute-parallel structure {:cmd         "turn off"
-                                       :coordinates [1]})
-      (sol/execute-parallel structure {:cmd         "turn on"
-                                       :coordinates [0]})
-      (is (= [1 0 0] (into [] structure))))))
+  (let [structure (transient [false false false])
+        worker-count (atom 0)]
+    (sol/execute-parallel worker-count structure {:cmd         "toggle"
+                                                  :coordinates [1]})
+    (sol/execute-parallel worker-count structure {:cmd         "turn off"
+                                                  :coordinates [1]})
+    (sol/execute-parallel worker-count structure {:cmd         "turn on"
+                                                  :coordinates [0]})
+    (is (= [true false false] (into [] (persistent! structure))))))
