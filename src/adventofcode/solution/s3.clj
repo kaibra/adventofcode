699a7392cb1f6c2c59d0c171a566905d8601363d
Kai Brandes <kai.brandes@otto.de>
2016-03-28 21:20:12 +0200

    solution 3b

--- src/adventofcode/solution/s3.clj
+++ src/adventofcode/solution/s3.clj
@@ -2,18 +2,38 @@
   (:require [clojure.java.io :as io]
             [adventofcode.solution.reading-files :as rf]))
 
-(defn do-step [visited {[x y] :pos} cmd]
+(defn do-step [visited [x y] cmd]
   (let [new-pos (case cmd
                   \^ [x (inc y)]
                   \v [x (dec y)]
                   \> [(inc x) y]
                   \< [(dec x) y])]
     (swap! visited #(conj % new-pos))
-    {:pos new-pos}))
+    new-pos))
 
-(defn start []
-  (println "Starting solution nr. 3")
+(defn start-santa [visited start-pos s]
+  (reduce (partial do-step visited) start-pos s))
+
+(defn when-even [i cmd]
+  (when (even? i) cmd))
+
+(defn when-not-even [i cmd]
+  (when-not (even? i) cmd))
+
+(defn starta []
+  (println "Starting solution nr. 3a")
   (with-open [rdr (io/reader "resources/3/input.txt")]
-    (let [visited (atom #{[0 0]})]
-      (reduce (partial do-step visited) {:pos [0 0]} (rf/char-seq rdr))
+    (let [start-pos [0 0]
+          visited (atom #{start-pos})]
+      (start-santa visited start-pos (rf/char-seq rdr))
+      (println (count @visited)))))
+
+(defn startb []
+  (println "Starting solution nr. 3b")
+  (with-open [rdr (io/reader "resources/3/input.txt")]
+    (let [the-input (rf/char-seq rdr)
+          start-pos [0 0]
+          visited (atom #{start-pos})]
+      (start-santa visited start-pos (keep-indexed when-even the-input))
+      (start-santa visited start-pos (keep-indexed when-not-even the-input))
       (println (count @visited)))))
