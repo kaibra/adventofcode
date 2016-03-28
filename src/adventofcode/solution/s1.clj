2f70c0774372895c846c1322a18b8152eddac709
Kai Brandes <kai.brandes@otto.de>
2016-03-28 17:22:39 +0200

    solution for problem 1

--- src/adventofcode/solution/s1.clj
+++ src/adventofcode/solution/s1.clj
@@ -1,4 +1,24 @@
-(ns adventofcode.solution.s1)
+(ns adventofcode.solution.s1
+  (:require [clojure.java.io :as io])
+  (:import (java.io Reader)))
+
+(defn char-seq [^Reader rdr]
+  (let [c (.read rdr)]
+    (if (>= c 0)
+      (cons (char c) (lazy-seq (char-seq rdr)))
+      [])))
+
+(defn apply-cmd [floor cmd]
+  (case cmd
+    \( (inc floor)
+    \) (dec floor)))
 
 (defn start []
-  )
+  (println "Starting solution nr. 1")
+  (with-open [rdr (io/reader "resources/1/input.txt")]
+    (-> (loop [the-seq (char-seq rdr)
+               floor 0]
+          (if (empty? the-seq)
+            floor
+            (recur (rest the-seq) (apply-cmd floor (first the-seq)))))
+        (println))))
