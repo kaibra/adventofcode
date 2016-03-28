aea594f33954184ec9cef4aaf5a89b64eef2537a
Kai Brandes <kai.brandes@otto.de>
2016-03-28 21:09:53 +0200

    mutable state for 3

--- src/adventofcode/solution/s3.clj
+++ src/adventofcode/solution/s3.clj
@@ -2,20 +2,18 @@
   (:require [clojure.java.io :as io]
             [adventofcode.solution.reading-files :as rf]))
 
-(defn do-step [{:keys [visited] [x y] :pos} cmd]
+(defn do-step [visited {[x y] :pos} cmd]
   (let [new-pos (case cmd
                   \^ [x (inc y)]
                   \v [x (dec y)]
                   \> [(inc x) y]
                   \< [(dec x) y])]
-    {:pos     new-pos
-     :visited (conj visited new-pos)}))
+    (swap! visited #(conj % new-pos))
+    {:pos new-pos}))
 
 (defn start []
   (println "Starting solution nr. 3")
   (with-open [rdr (io/reader "resources/3/input.txt")]
-    (-> (reduce do-step {:pos     [0 0]
-                         :visited #{[0 0]}} (rf/char-seq rdr))
-        :visited
-        (count)
-        (println))))
+    (let [visited (atom #{[0 0]})]
+      (reduce (partial do-step visited) {:pos [0 0]} (rf/char-seq rdr))
+      (println (count @visited)))))
